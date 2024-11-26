a593376ff60f833bef62f2f9fc544cec1b2faa61
Your Name <you@example.com>
2024-11-26 12:21:18 +0100

    adding tests to the workflow

--- end-to-end-test.sh
+++ end-to-end-test.sh
@@ -2,9 +2,23 @@
 
 set -e
 
+# Export OpenStack credentials from environment variables
+export OS_AUTH_URL=$OS_AUTH_URL
+export OS_USERNAME=$OS_USERNAME
+export OS_PASSWORD=$OS_PASSWORD
+export OS_PROJECT_NAME=$OS_PROJECT_NAME
+export OS_PROJECT_ID=$OS_PROJECT_ID
+export OS_USER_DOMAIN_NAME=$OS_USER_DOMAIN_NAME
+export OS_PROJECT_DOMAIN_ID=$OS_PROJECT_DOMAIN_ID
+export OS_REGION_NAME=$OS_REGION_NAME
+export OS_IDENTITY_API_VERSION=$OS_IDENTITY_API_VERSION
+export OS_INTERFACE=$OS_INTERFACE
+
+# Fetch the floating IP
 FLOATING_IP=$(openstack floating ip list --status ACTIVE -f value -c "Floating IP Address" | head -n 1)
 
-if curl -s "http://$FLOATING_IP" | grep -q "Apache2 Ubuntu Default Page"; then
+# Perform a simple end-to-end check
+if curl -s "http://$FLOATING_IP" | grep -q "ToDo App"; then
   echo "End-to-end test passed - Application is accessible"
 else
   echo "End-to-end test failed - Application is not accessible"
--- integration-test.sh
+++ integration-test.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+export OS_AUTH_URL=$OS_AUTH_URL
+export OS_USERNAME=$OS_USERNAME
+export OS_PASSWORD=$OS_PASSWORD
+export OS_PROJECT_NAME=$OS_PROJECT_NAME
+export OS_PROJECT_ID=$OS_PROJECT_ID
+export OS_USER_DOMAIN_NAME=$OS_USER_DOMAIN_NAME
+export OS_PROJECT_DOMAIN_ID=$OS_PROJECT_DOMAIN_ID
+export OS_REGION_NAME=$OS_REGION_NAME
+export OS_IDENTITY_API_VERSION=$OS_IDENTITY_API_VERSION
+export OS_INTERFACE=$OS_INTERFACE
+
 FLOATING_IP=$(openstack floating ip list --status ACTIVE -f value -c "Floating IP Address" | head -n 1)
 
 if ping -c 4 "$FLOATING_IP" >/dev/null 2>&1; then
